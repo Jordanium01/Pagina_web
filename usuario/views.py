6c0edd2d3361bf5468182be47316db3edd3320c4
Jordanium01 <102256248+Jordanium01@users.noreply.github.com>
2022-06-27 16:45:45 -0400

    Delete usuario directory

--- usuario/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def login(request):
-		return render(request,'usuario/login.html')
-
-def regis(request):
-		return render(request,'usuario/registro.html')
