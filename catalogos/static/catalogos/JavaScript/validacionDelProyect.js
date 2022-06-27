ef291cf6120fd35dee8ec71eb24917c370679065
DexterGr1f <102192880+DexterGr1f@users.noreply.github.com>
2022-06-27 18:19:31 -0400

    Add files via upload

---
+++ catalogos/static/catalogos/JavaScript/validacionDelProyect.js
@@ -0,0 +1,3 @@
+function clicked(){
+    return alert("aiuda");
+}
---
+++ catalogos/static/catalogos/JavaScript/validacionProyectos.js
@@ -0,0 +1,12 @@
+//Confirmar al eliminar un proyecto
+function confirmar() {
+    var Respuesta = confirm("¿Estas seguro de querer eliminar?")
+
+    if (Respuesta == true) {
+
+        return true;
+    } else {
+
+        return false;
+    }
+}
