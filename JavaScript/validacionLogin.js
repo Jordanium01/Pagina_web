f5794aaf73284bfa47eab463a14859f7354c05dd
DexterGr1f <102192880+DexterGr1f@users.noreply.github.com>
2022-06-27 18:07:59 -0400

    primer avance

--- JavaScript/validacionLogin.js
+++ JavaScript/validacionLogin.js
@@ -2,73 +2,56 @@
 var expr = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 $(document).ready(() => {
-    var CheckEmail = false;
-    var CheckPassword = false;
-
-    $("#form_email").blur(function(){
-        CheckEmail = verifyEmail("#form_email");
-     });
-
-    $("#form_password").blur(function(){
-        CheckPassword = verifyPassword("#form_password");
-    });
-
-    //Validar Cuando se envie el formulario
+    //$("#errorCorreo").hide();
     $("#form_login").submit(function() {
-        CheckEmail = verifyEmail("#form_email");
-        CheckPassword = verifyPassword("#form_password");
+        //alert("formulario");
+        var correo = $("#form_email").val().trim();
+        var password = $("#form_password").val().trim();
+        var CheckEmail = false;
+        var CheckPassword = false
+
+        if (correo.length == 0 || !expr.test(correo)) {
+            //Volver la parte del css en visible
+            $('#errorCorreo').css("visibility", "visible");
+            //Ponerle los bordes rojos
+            $('#form_email').css("border-color", "var(--color-error)");
+            //Reemplazar el mensaje de error
+            if (correo.length == 0) { $("#errorCorreo").text("No puede dejar su correo en blanco"); } else { $("#errorCorreo").text("No ingreso un correo válido"); }
+            //Establecer los iconos
+            $("#I_correo_Succes").css("visibility", "hidden");
+            $("#I_correo_Error").css("visibility", "visible");
+        } else { //Si el correo si fue validado
+            $('#errorCorreo').css("visibility", "hidden");
+            $('#form_email').css("border-color", "var(--color-success)");
+            $("#I_correo_Succes").css("visibility", "visible");
+            $("#I_correo_Error").css("visibility", "hidden");
+            CheckEmail = true;
+        }
+
+        if (password.length == 0) {
+            //Volver la parte del css en visible
+            $('#errorPassw').css("visibility", "visible");
+            //Ponerle los bordes rojos
+            $('#form_password').css("border-color", "var(--color-error)");
+            //Reemplazar el mensaje de error
+            $("#errorPassw").text("No puede dejar la contraseña en blanco");
+            //Establecer los iconos
+            $("#I_pass_Succes").css("visibility", "hidden");
+            $("#I_pass_Error").css("visibility", "visible");
+        } else {
+            //Volver la parte del css invisible
+            $('#errorPassw').css("visibility", "hidden");
+            //Ponerle los bordes verdes
+            $('#form_password').css("border-color", "var(--color-success)");
+            //Establecer los iconos
+            $("#I_pass_Succes").css("visibility", "visible");
+            $("#I_pass_Error").css("visibility", "hidden");
+            CheckPassword = true;
+        }
 
         if (CheckEmail == false || CheckPassword == false) {
-            //Evitar que el envio se realice
             return false;
         }
 
     });
 });
-
-
-//Validar campo de correo
-function verifyEmail(emailID){
-    var correo = $(emailID).val().trim();
-    if (correo.length == 0 || !expr.test(correo)) {
-        //Reemplazar el mensaje de error
-        if (correo.length == 0) { 
-            setError(emailID, "#form_control_email", "#errorCorreo", "-No puede dejar su correo en blanco");
-            return false;
-        } else{ 
-            setError(emailID, "#form_control_email", "#errorCorreo", "-Correo ingresado invalido");
-            return false;
-        }
-    } else { //Si el correo si fue validado
-        setSucces(emailID, "#form_control_email");
-        return true;
-    }
-}
-
-function verifyPassword(passID){
-    var password = $(passID).val().trim();
-    if (password.length == 0){
-        setError(passID, "#form_control_password", "#errorPassw", "-No puede dejar su contraseña en blanco");
-        return false;
-    } else {
-        setSucces(passID, "#form_control_password");
-        return true;
-    }
-}
-
-//Establecer que la seccion esta mal
-function setError(intputID, controlID, messageID, message){
-    $(intputID).css("border-color", "var(--color-error)");
-    //Quitar clase (si no esta no la quita)
-    $(controlID).removeClass("success");
-    //Poner la buena
-    $(controlID).addClass("error");
-    $(messageID).text(message);
-}
-
-//Establecer que la seccion esta bien
-function setSucces(intputID, controlID){
-    $(intputID).css("border-color", "var(--color-success)");
-    $(controlID).removeClass("error");
-    $(controlID).addClass("success");
-}
