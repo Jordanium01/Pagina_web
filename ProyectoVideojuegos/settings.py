877be32969b3716cd34e00fd9737153e21db4c5a
DexterGr1f <102192880+DexterGr1f@users.noreply.github.com>
2022-07-10 20:35:19 -0400

    oracle

--- ProyectoVideojuegos/settings.py
+++ ProyectoVideojuegos/settings.py
@@ -11,13 +11,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+'''BASE_DIR = Path(__file__).resolve().parent.parent'''
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nk@3o1m0c7$y89wa0jv*9=)xmf1vzei-ripbd@ej1tns48@t&y'
@@ -91,7 +100,7 @@ ROOT_URLCONF = 'ProyectoVideojuegos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], #cambiado para lo de google
+        'DIRS': [], #cambiado para lo de google
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,9 +121,16 @@ WSGI_APPLICATION = 'ProyectoVideojuegos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##almacen',
+        'PASSWORD': 'almacen',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 # Password validation
